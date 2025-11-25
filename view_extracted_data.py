4b956b93487b9440f5ec5747fda1a0e6530a12da
Filip <szczepanski.grzegorz.filip@gmail.com>
2025-11-25 23:19:05 +0000

    small fix

--- view_extracted_data.py
+++ view_extracted_data.py
@@ -5,8 +5,9 @@ arr = np.load("extracted_data/sim_L3.00_D0.25_U0.25_Ref0.npy")
 print(arr.shape)
 
 "Output Format: [x, y, z, u, v, w, p, y_wall, is_fluid, is_wall, is_inlet, is_outlet]"
-# print wall distance
-for i in range(1000):
-    print(arr[i,7])
+#                0  1  2  3  4  5  6    7         8        9        10         11
+
+for i in range(10000):
+    print(arr[i,10])
 
 print("done")
